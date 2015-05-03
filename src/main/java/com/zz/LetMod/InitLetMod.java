154789ef6b14aa6aad827d980be1941ef7ec3677
ZoomPoppy <514027735@qq.com>
2015-05-03 22:00:22 +0800

    代码初始化，以及空mod

---
+++ src/main/java/com/zz/LetMod/InitLetMod.java
@@ -0,0 +1,25 @@
+package com.zz.LetMod;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by zz on 2015/5/2.
+ */
+
+@Mod(modid = "LetMod",name = "let mod",version = "1.7.20-1.0")
+public class InitLetMod {
+    @Mod.Instance
+    public static InitLetMod instance;
+    @Mod.EventHandler
+    public void perInit(){
+
+    }
+    @Mod.EventHandler
+    public void Init(){
+
+    }
+    @Mod.EventHandler
+    public void posetInit(){
+
+    }
+}
