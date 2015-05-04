27a586dbfe947c7b10ca0a92db44b63a931aa5eb
ZoomPoppy <514027735@qq.com>
2015-05-04 13:34:54 +0800

    成功解决了mcmod.info没有作用的问题， 可能是写的不对，也可能是gradle版本的问题

--- src/main/java/com/zz/LetMod/InitLetMod.java
+++ src/main/java/com/zz/LetMod/InitLetMod.java
@@ -1,25 +1,28 @@
 package com.zz.LetMod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 /**
  * Created by zz on 2015/5/2.
  */
 
-@Mod(modid = "LetMod",name = "let mod",version = "1.7.20-1.0")
+@Mod(modid = "LetMod",name = "let mod",version = "1.7.10-1.0")
 public class InitLetMod {
     @Mod.Instance
     public static InitLetMod instance;
     @Mod.EventHandler
-    public void perInit(){
+    public void preInit(FMLPreInitializationEvent event){
 
     }
     @Mod.EventHandler
-    public void Init(){
+    public void Init(FMLInitializationEvent event){
 
     }
     @Mod.EventHandler
-    public void posetInit(){
+    public void posetInit(FMLPostInitializationEvent event){
 
     }
 }
