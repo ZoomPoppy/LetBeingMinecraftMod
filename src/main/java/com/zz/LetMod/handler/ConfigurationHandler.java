4164e3faa8e4044eae4d45d7415398e6da904301
ZoomPoppy <514027735@qq.com>
2015-05-04 22:46:57 +0800

    改进config加载和保存

--- src/main/java/com/zz/LetMod/handler/ConfigurationHandler.java
+++ src/main/java/com/zz/LetMod/handler/ConfigurationHandler.java
@@ -14,32 +14,26 @@ public class ConfigurationHandler {
 
     public static Configuration configuration;
 
+    public static boolean configValue = false;
 
     public static void init(File configFile){
+        if (configuration == null){
+            configuration = new Configuration(configFile);
 
-        configuration = new Configuration(configFile);
-
-        boolean configValue = false;
-        try {
-            //load
-            configuration.load();
-             configValue = configuration.get(configuration.CATEGORY_GENERAL,
-                    "comfigValue",true,"this is an example config value").getBoolean(true);
-        }catch(Exception e){
-            //log the exception
         }
-        finally {
-            //save the handler file
-            if (configuration.hasChanged()){
-                configuration.save();
 
-            }
-        }
     }
+
     @Subscribe
     public void onConfigurationChangeEvent(ConfigChangedEvent.OnConfigChangedEvent event){
         if (event.modID.equalsIgnoreCase(Reference.MOD_ID)){
-            //resync configs
+             loadConfiguaration();
+        }
+    }
+    public void loadConfiguaration(){
+        configValue = configuration.getBoolean("configVaule",Configuration.CATEGORY_GENERAL,false,"this is an example configuration value");
+        if (configuration.hasChanged()){
+            configuration.save();
         }
     }
 }
